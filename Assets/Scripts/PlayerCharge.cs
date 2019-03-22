1f71d8acc50975622aba8b2cc0128490861518fd
Ben Mallinson <ben@benmallinson.me>
2019-03-22 21:10:54 +0000

    refining movement and adding charge bar

---
+++ Assets/Scripts/PlayerCharge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCharge : MonoBehaviour {
+    public int currentCharge;
+    public Slider chargeSlider;
+
+    void Awake () {
+        chargeSlider.gameObject.SetActive (false);
+    }
+
+    public void setCharge (int charge) {
+        if (currentCharge == 0 && charge > 0) chargeSlider.gameObject.SetActive (true);
+        if (currentCharge > 0 && charge == 0) chargeSlider.gameObject.SetActive (false);
+        currentCharge = charge;
+        chargeSlider.value = charge;
+    }
+
+    public void setMax (int max) {
+        chargeSlider.maxValue = max;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody2D rigidBody;
+    public float thrust;
+    private int chargeTimer = 0;
+    public int maxCharge;
+
+    public GameObject playerStart;
+
+    public PlayerCharge playerCharge;
+
+    void Awake () {
+        playerCharge = gameObject.GetComponent<PlayerCharge> ();
+        playerCharge.setMax (maxCharge);
+    }
+
+    void Start () {
+        rigidBody = gameObject.GetComponent<Rigidbody2D> ();
+        thrust = gameObject.GetComponent<float> ();
+        maxCharge = gameObject.GetComponent<int> ();
+        playerStart = gameObject.GetComponent<GameObject> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        processMovement ();
+    }
+
+    void processMovement () {
+        if (movementKeyDown () && chargeTimer <= maxCharge) {
+            chargeTimer++;
+            playerCharge.setCharge (chargeTimer);
+        }
+
+        if (Input.GetKeyUp ("right")) {
+            addForce (new Vector2 (1, 3));
+            resetChargeTimer ();
+        } else if (Input.GetKeyUp ("left")) {
+            addForce (new Vector2 (-1, 3));
+            resetChargeTimer ();
+        } else if (Input.GetKeyUp ("up")) {
+            addForce (new Vector2 (0, 4));
+            resetChargeTimer ();
+        }
+    }
+
+    void resetChargeTimer () {
+        chargeTimer = 0;
+        playerCharge.setCharge (chargeTimer);
+    }
+
+    void addForce (Vector2 vector2) {
+        rigidBody.AddForce (vector2 * ((chargeTimer * 2) + thrust));
+    }
+
+    bool movementKeyDown () {
+        return Input.GetKey ("right") || Input.GetKey ("left") || Input.GetKey ("up");
+    }
+}
